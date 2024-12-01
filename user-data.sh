19d68dd26465142cb72058eaa280e8a3d350798f
Haritch-6509650757 <144800381+Haritch-6509650757@users.noreply.github.com>
2024-12-02 05:37:51 +0700

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -2,7 +2,9 @@
 # Update the package manager and install Docker
 sudo yum update -y
 sudo yum install -y docker
+# Start docker serivce
 sudo service docker start
 sudo usermod -aG docker ec2-user
+sudo service docker restart
 
 
