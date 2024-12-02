683988a52a0ae6c968f64291b8c0d7a6cf9943be
Haritch-6509650757 <144800381+Haritch-6509650757@users.noreply.github.com>
2024-12-02 18:32:03 +0700

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -9,3 +9,6 @@ sudo usermod -aG docker ec2-user
 #create necessary directories for Strapi
 sudo mkdir -P /app/public/uploads
 sudo chmod -R 775 /app/public/uploads
+
+# Restart docker service to apply change
+sudo service docker restart
